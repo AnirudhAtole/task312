4d1aa21026725cdf37a7ac04d0e80cdba407c258
Anirudh Atole <anirudhatole@gmail.com>
2023-01-07 20:00:01 +0530

    task 506 del from webpage done

--- book_app/main.js
+++ book_app/main.js
@@ -2,6 +2,7 @@ var sub = document.getElementById("my-form");
 var user_list = document.getElementById("user");
 
 sub.addEventListener('submit',store_details);
+user_list.addEventListener('click',remove_user);
 
 function store_details(e)
 {
@@ -15,11 +16,30 @@ function store_details(e)
 
     let my_obj_serialized = JSON.stringify(my_obj);
 
+    //creating user list
     let user = document.createElement('li');
+    //creating tet node of li
     user.appendChild(document.createTextNode(`name:- ${my_obj.name}  email id:- ${my_obj.email}  phone_number:- ${my_obj.phone_number}`));
+
+    //creating a delete button
+    var del_butt = document.createElement('button');
+    del_butt.appendChild(document.createTextNode("delete"));
+    del_butt.className = 'btn1';
+    del_butt.onclick = "remove_user()"; 
+
+    user.appendChild(del_butt);
+
+    //appending to the list
     user_list.appendChild(user);
 
     localStorage.setItem(document.getElementById("email").value,my_obj_serialized);
+}
 
-
+function remove_user(e)
+{
+    if(confirm("Do you want to delete the booking"))
+    {
+         var li = e.target.parentElement;
+        user_list.removeChild(li);
+    }
 }
